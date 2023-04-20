e4f8096c6d7974a4187f3a2fbef7d09965f84b50
nsquare3 <113948443+nsquare3@users.noreply.github.com>
2023-04-19 19:08:31 -0700

    small portal improvements (#22)

--- src/definitions.ts
+++ src/definitions.ts
@@ -18,11 +18,11 @@ export function getPortalBaseURL(ctx: Ctx) {
     case Environment.DEV:
       return 'http://localhost:3001';
     case Environment.SANDBOX:
-      return 'https://portal.sandbox.usecapsule.com';
+      return 'https://app.sandbox.usecapsule.com';
     case Environment.BETA:
-      return 'https://portal.beta.usecapsule.com';
+      return 'https://app.beta.usecapsule.com';
     case Environment.PROD:
-      return 'https://portal.usecapsule.com';
+      return 'https://app.usecapsule.com';
     default:
       throw new Error(`env: ${env} not supported`);
   }
