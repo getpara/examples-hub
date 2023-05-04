505deff103bb92e2a5ddc1a6b2c574144b3a09b3
osdnk <micosa97@gmail.com>
2023-05-04 13:58:32 -0700

    export modal :3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Capsule } from './Capsule';
 export { Environment } from './definitions';
+import { CapsuleModal as Modal } from "./modal/CapsuleModal"
 export default Capsule;
+export { Modal };
 
 global.Buffer = global.Buffer || require('buffer').Buffer;
