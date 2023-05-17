cc7d36b041e91fb5abd48bb5b064e5338756ad53
osdnk <micosa97@gmail.com>
2023-05-16 18:15:14 -0700

    TX details

--- portal/src/TransactionReview.tsx
+++ portal/src/TransactionReview.tsx
@@ -5,11 +5,13 @@ import {
   Button,
   Container,
   Text,
-  ChakraProvider,
+  ChakraProvider, Box, Spacer, VStack, Flex,
 } from '@chakra-ui/react';
 
 import { userManagementClient } from './userManagementClient';
 import { generateSignature } from './library/cryptography/webAuth';
+import CapsuleBox from "./assets/CapsuleBox";
+import PoweredByCapsule from "./assets/poweredByCapsule";
 
 function TransactionReview() {
   const { userId, pendingTransactionId } = useParams();
@@ -17,6 +19,7 @@ function TransactionReview() {
   const email = searchParams.get('email');
   const [pendingTransaction, setPendingTransaction] = useState(null);
 
+  const partnerName = pendingTransaction.partner.name
   async function onClickAccept() {
     const { data } = await userManagementClient.getWebChallenge(
       decodeURIComponent(email),
@@ -56,23 +59,52 @@ function TransactionReview() {
   }
   return (
     <ChakraProvider>
+      <Box height="100%" padding="32px">
+        <VStack alignItems="center" color="white" maxW="ld">
+          <Text fontSize="m" textAlign="center">
+            <b>{partnerName}</b> is requesting access to perform the following
+            operation on your wallet
+          </Text>
+          <Text mb={8} fontSize="xs" textAlign="center">
+            Please only proceed if you trust {partnerName}.
+          </Text>
+          <Box height="60px" alignItems="center" display="flex">
+            <CapsuleBox />
+          </Box>
+          <Spacer />
+        </VStack>
+      </Box>
       <Container color="white" maxW="ld" padding={10}>
-        <Heading size="xl" mb={8}>
-          Transaction Review
-        </Heading>
-        <Text mb={8}>{JSON.stringify(pendingTransaction, null, 2)}</Text>
-        <Text mb={8}>If the transaction looks good, click accept below.</Text>
+        <Text fontFamily={"monospace"} mb={8}>{JSON.stringify(pendingTransaction.decodedTx, null, 2)}</Text>
 
         <Container width="100%" display="flex" justifyContent="center">
           <Button
+            colorScheme="whiteAlpha"
+            onClick={() => {
+              window.close();
+            }}
+            size="md"
+            alignSelf={'center'}
+            width="140px"
+          >
+            Deny
+          </Button>
+          <Button
+            width="140px"
+            marginLeft="10px"
             colorScheme="green"
             onClick={onClickAccept}
-            size="lg"
+            size="md"
             alignSelf={'center'}
           >
-            Accept Transaction
+            Accept
           </Button>
         </Container>
+        <Box height="62px" width="100%">
+          <Flex h="57px" w="100%" justifyContent={'center'} alignItems={'center'}>
+            <PoweredByCapsule w={50} h={20} />
+          </Flex>
+        </Box>
       </Container>
     </ChakraProvider>
   );
