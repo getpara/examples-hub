685f55dd5c8c11e9b115ccc3fa2d73b2496d867c
nsquare3 <113948443+nsquare3@users.noreply.github.com>
2023-12-18 08:16:49 -0800

    remove import of elliptic library (#168)

--- core/src/shares/KeyContainer.ts
+++ core/src/shares/KeyContainer.ts
@@ -1,71 +1,9 @@
 import {
   Encrypt as ECIESEncrypt,
-  AES128DecryptAndHMAC,
+  Decrypt as ECIESDecrypt,
 } from '@celo/utils/lib/ecies';
 import * as eutil from 'ethereumjs-util';
-import * as elliptic from 'elliptic';
-import { createHash } from 'crypto-browserify';
 import * as forge from 'node-forge';
-// import { Buffer } from 'buffer';
-
-const ec = new elliptic.ec('secp256k1');
-
-/**
- * Manages information for a keyshare that must be persisted
- * Helps with encryption and decryption of Capsule backups
- */
-
-const IncCounter = (ctr: Buffer) => {
-  for (let i = ctr.length - 1; i >= 0; i--) {
-    ctr[i]++;
-    if (ctr[i] !== 0) {
-      return ctr;
-    }
-  }
-  return ctr;
-};
-
-const ConcatKDF = (px: Buffer, kdLen: number) => {
-  const blockSize = 32;
-  const reps = ((kdLen + 7) * 8) / (blockSize * 8);
-  let counter = Buffer.from('00000001', 'hex');
-  let k = Buffer.from('00', 'hex');
-  for (let i = 0; i <= reps; i++) {
-    const hash = createHash('sha256');
-    hash.update(counter);
-    hash.update(px);
-    k = Buffer.concat([k, hash.digest()]);
-    counter = IncCounter(counter);
-  }
-  return k.slice(1, kdLen + 1);
-};
-
-/**
- * ECIES decrypt
- * @param {Buffer} privKey Ethereum private key, 32 bytes.
- * @param {Buffer} encrypted Encrypted message, serialized, 113+ bytes
- * @returns {Buffer} plaintext
- */
-export function ECIESDecrypt(privKey: Buffer, encrypted: Buffer) {
-  // Read iv, ephemPubKey, mac, ciphertext from encrypted message
-  const ephemPubKeyEncoded = encrypted.slice(0, 65);
-  const symmetricEncrypted = encrypted.slice(65);
-
-  // NOTE: elliptic is disabled elsewhere in this library to prevent
-  // accidental signing of truncated messages.
-  // tslint:disable-next-line:import-blacklist
-  // const EC = require('elliptic').ec
-  // const ec = new EC('secp256k1')
-
-  const ephemPubKey = ec.keyFromPublic(ephemPubKeyEncoded).getPublic();
-  const px = ec.keyFromPrivate(privKey).derive(ephemPubKey);
-  const hash = ConcatKDF(px.toArrayLike(Buffer), 32);
-  // km, ke
-  const encryptionKey = hash.slice(0, 16);
-  const macKey = createHash('sha256').update(hash.slice(16)).digest();
-
-  return AES128DecryptAndHMAC(encryptionKey, macKey, symmetricEncrypted);
-}
 
 export class KeyContainer {
   public walletId: string;
--- core/src/transmission/transmissionUtils.ts
+++ core/src/transmission/transmissionUtils.ts
@@ -1,21 +1,13 @@
 import { randomBytes } from 'crypto';
-import { ec as EC } from 'elliptic';
-import { Encrypt as ECIESEncrypt } from '@celo/utils/lib/ecies';
+import { Encrypt as ECIESEncrypt, Decrypt as ECIESDecrypt } from '@celo/utils/lib/ecies';
 import { Buffer } from 'buffer';
+import * as eutil from 'ethereumjs-util';
 import Client from '@usecapsule/user-management-client';
-import { ECIESDecrypt } from '../shares/KeyContainer';
 
 export async function upload(message: string, userManagementClient: Client) {
   const secret = randomBytes(32).toString('hex');
-  const ec = new EC('secp256k1');
-  const privKey = ec.keyFromPrivate(Buffer.from(secret, 'hex'));
-  const pubKey = privKey.getPublic(false, 'hex');
-  const publicKey = Buffer.from(pubKey, 'hex');
-  const pubkey = Buffer.from(
-    ec.keyFromPublic(publicKey).getPublic(false, 'hex'),
-    'hex',
-  ).subarray(1);
-  const data = ECIESEncrypt(Buffer.from(pubkey), Buffer.from(message, 'ucs2')).toString(
+  const pubkey = Buffer.from(eutil.privateToPublic(Buffer.from(secret, 'hex')));
+  const data = ECIESEncrypt(pubkey, Buffer.from(message, 'ucs2')).toString(
     'base64',
   );
 
