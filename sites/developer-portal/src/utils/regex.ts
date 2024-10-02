b393edce5c4d56381778f0dc19588867d715ef65
Taylor Bosch <43397354+tbosch82@users.noreply.github.com>
2024-10-01 23:20:12 -0500

    Dev Portal - Update TLD Max Length (#439)

--- sites/developer-portal/src/utils/regex.ts
+++ sites/developer-portal/src/utils/regex.ts
@@ -4,6 +4,6 @@ export const TWITTER_URL_REGEX = /^https:\/\/(www.)?(twitter|x)\.com\/.+/i;
 export const LINKEDIN_URL_REGEX = /^https:\/\/(www.)?linkedin\.com\/company\/.+/i;
 export const GITHUB_URL_REGEX = /^https:\/\/(www.)?github\.com\/.+/i;
 
-export const HTTPS_URL_REGEX = /^(https:\/\/)([\da-z.-]+\.[a-z.]{2,6}|[\d.]+)([/:?=&#]{1}[\da-z.-]+)*[/?]?$/gim;
+export const HTTPS_URL_REGEX = /^(https:\/\/)([\da-z.-]+\.[a-z.]{2,63}|[\d.]+)([/:?=&#]{1}[\da-z.-]+)*[/?]?$/gim;
 
 export const EMAIL_REGEX = /([\w.\-_]+)?\w+@[\w-_]+(\.\w+){1,}/gim;
