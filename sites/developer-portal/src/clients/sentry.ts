6f252c52fb4a5a86e32ff1360bdee28d540d3040
Taylor Bosch <43397354+tbosch82@users.noreply.github.com>
2024-10-08 11:47:23 -0500

    Dev Portal - Fix Sentry DSN (#462)

--- sites/developer-portal/src/clients/sentry.ts
+++ sites/developer-portal/src/clients/sentry.ts
@@ -2,7 +2,7 @@ import { init, extraErrorDataIntegration } from '@sentry/react';
 import { ENV_VARS } from '../utils/constants';
 
 init({
-  dsn: 'https://1539dfd114264da8b8675e9c49310c26@o4504568036720640.ingest.us.sentry.io/4508054352953344',
+  dsn: 'https://4cbbbada7a241787f984587db5d98741@o4504568036720640.ingest.us.sentry.io/4508054352953344',
   environment: ENV_VARS.environment.toLowerCase(),
   integrations: [extraErrorDataIntegration()],
 });
