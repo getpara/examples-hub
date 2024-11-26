17432db9f843f3b4f5e5619f8183e476e021a39c
Norwood <113948443+nsquare3@users.noreply.github.com>
2024-11-26 14:51:04 -0500

    fix: better esm support for core-components (#565)

--- packages/core-components/scripts/appendLoaderExports.ts
+++ packages/core-components/scripts/appendLoaderExports.ts
@@ -18,6 +18,18 @@
     // Append to the main types file
     await fs.appendFile(distTypesPath, exportStatementTypes, 'utf8');
     console.log(`Successfully appended type exports to ${distTypesPath}`);
+
+    const loaderRelativePackageJsonPath = './dist/loader/package.json';
+    const customPackageJson = {
+      name: 'capsule-loader',
+      private: true,
+      types: './index.d.ts',
+      type: 'module',
+      main: './index.js',
+    };
+
+    await fs.writeFile(loaderRelativePackageJsonPath, JSON.stringify(customPackageJson, null, 2));
+    console.log('Custom package.json written to loader directory.');
   } catch (error) {
     console.error('Error appending loader exports:', error);
     process.exit(1);
