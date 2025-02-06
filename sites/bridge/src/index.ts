d002b966df2efc99c4d1db1e3713afd773d98b5f
Brian Corbin <brian.william.corbin@gmail.com>
2025-02-06 09:17:24 -0800

    fix: adding missing fns and reordering alphabetically (#682)

--- sites/bridge/src/index.ts
+++ sites/bridge/src/index.ts
@@ -98,6 +98,58 @@ async function invokeParaMethod(methodName: string, args: any[], requestId: stri
   try {
     const para = window['para'] as ParaWeb;
     switch (methodName) {
+      case 'check2FAStatus': {
+        console.log('Checking 2FA status...');
+        const is2FAEnabled = await para.check2FAStatus();
+        sendResponse('check2FAStatus', requestId, is2FAEnabled);
+        break;
+      }
+      case 'checkIfUserExists': {
+        console.log('Checking if user exists...');
+        const userExists = await para.checkIfUserExists({ email: args[0] });
+        sendResponse('checkIfUserExists', requestId, userExists);
+        break;
+      }
+      case 'checkIfUserExistsByPhone': {
+        console.log('Checking if user exists by phone...');
+        const userExistsByPhone = await para.checkIfUserExistsByPhone({ phone: args[0], countryCode: args[1] });
+        sendResponse('checkIfUserExistsByPhone', requestId, userExistsByPhone);
+        break;
+      }
+      case 'claimPregenWallets': {
+        console.log('Claiming pregen wallets...');
+        const claimResult = await para.claimPregenWallets({ pregenIdentifier: args[0], pregenIdentifierType: args[1] });
+        sendResponse('claimPregenWallets', requestId, claimResult);
+        break;
+      }
+      case 'clearStorage': {
+        console.log('Clearing storage...');
+        para.clearStorage();
+        sendResponse('clearStorage', requestId, null);
+        break;
+      }
+      case 'createPregenWalletPerType': {
+        console.log('Creating pregen wallet per type...');
+        const pregenWallets = await para.createPregenWalletPerType({
+          pregenIdentifier: args[0],
+          pregenIdentifierType: args[1],
+          types: args[2],
+        });
+        sendResponse('createPregenWalletPerType', requestId, pregenWallets);
+        break;
+      }
+      case 'createUser': {
+        console.log('Creating user...');
+        await para.createUser({ email: args[0] });
+        sendResponse('createUser', requestId, null);
+        break;
+      }
+      case 'createUserByPhone': {
+        console.log('Creating user by phone...');
+        await para.createUserByPhone({ phone: args[0], countryCode: args[1] });
+        sendResponse('createUserByPhone', requestId, null);
+        break;
+      }
       case 'createWallet': {
         console.log('Creating wallet...');
         const [wallet, recoveryShare] = await para.createWallet({ type: args[0], skipDistribute: args[1] });
@@ -105,55 +157,32 @@ async function invokeParaMethod(methodName: string, args: any[], requestId: stri
         sendResponse('createWallet', requestId, [{ wallet, recoveryShare }]);
         break;
       }
-      case 'generatePasskey': {
-        console.log('Generating passkey...');
-        await generatePasskey(para, args);
-        console.log('Passkey generated successfully.');
-        sendResponse('generatePasskey', requestId, true);
+      case 'createWalletPerType': {
+        console.log('Creating wallet per type...');
+        const wallets = await para.createWalletPerType({ skipDistribute: args[0], types: args[1] });
+        sendResponse('createWalletPerType', requestId, wallets);
         break;
       }
-      case 'generatePasskeyV2': {
-        console.log('Generating passkey V2...');
-        await generatePasskeyV2(para, args);
-        console.log('Passkey V2 generated successfully.');
-        sendResponse('generatePasskeyV2', requestId, true);
+      case 'createWalletPreGen': {
+        console.log('Creating wallet pre-gen...');
+        const wallet = await para.createPregenWallet({
+          type: args[0],
+          pregenIdentifier: args[1],
+          pregenIdentifierType: args[2],
+        });
+        sendResponse('createWalletPreGen', requestId, wallet);
         break;
       }
-      case 'getWebChallenge': {
-        console.log('Getting web challenge...');
-        const getWebChallengeResult = await para.ctx.client.getWebChallenge(args[0] ?? { email: '' });
-        console.log('Web challenge result:', getWebChallengeResult);
-        sendResponse('getWebChallenge', requestId, getWebChallengeResult);
+      case 'distributeNewWalletShare': {
+        console.log('Distributing new wallet share...');
+        const distributeResult = await para.distributeNewWalletShare({ walletId: args[0], userShare: args[1] });
+        sendResponse('distributeNewWalletShare', requestId, distributeResult);
         break;
       }
-      case 'verifyWebChallenge': {
-        console.log('Verifying web challenge...');
-        const verifyWebChallengeResult = await verifyWebChallenge(para, args);
-        console.log('Web challenge verified:', verifyWebChallengeResult);
-        sendResponse(
-          'verifyWebChallenge',
-          requestId,
-          platform === Platform.iOS ? verifyWebChallengeResult['data']['userId'] : verifyWebChallengeResult,
-        );
-        break;
-      }
-      case 'login': {
-        console.log('Logging in...');
-        const desiredWallet = await login(para, args);
-        console.log('Login successful. Desired wallet:', desiredWallet);
-        sendResponse('login', requestId, desiredWallet);
-        break;
-      }
-      case 'loginV2': {
-        console.log('Logging in V2...');
-        const desiredWallet2 = await loginV2(para, args);
-        console.log('Login V2 successful. Desired wallet:', desiredWallet2);
-        sendResponse('loginV2', requestId, desiredWallet2);
-        break;
-      }
-      case 'initEthersSigner': {
-        await initEthersSigner(para, args);
-        sendResponse('initEthersSigner', requestId, true);
+      case 'enable2FA': {
+        console.log('Enabling 2FA...');
+        await para.enable2FA({ verificationCode: args[0] });
+        sendResponse('enable2FA', requestId, null);
         break;
       }
       case 'ethersSignMessage': {
@@ -174,13 +203,229 @@ async function invokeParaMethod(methodName: string, args: any[], requestId: stri
         sendResponse('ethersSendTransaction', requestId, txResponse);
         break;
       }
-      default: {
-        console.log(`Invoking default method: ${methodName}`);
-        const result = para[methodName](...args);
-        const resolvedResult = result instanceof Promise ? await result : result;
-        console.log(`Method ${methodName} invoked successfully. Result:`, resolvedResult);
-        sendResponse(methodName, requestId, resolvedResult);
+      case 'exportSession': {
+        console.log('Exporting session...');
+        const exportedSession = para.exportSession();
+        sendResponse('exportSession', requestId, exportedSession);
         break;
+      }
+      case 'externalWalletLogin': {
+        console.log('Logging in external wallet...');
+        const externalWalletLoginResult = await para.externalWalletLogin({ address: args[0], type: args[1] });
+        sendResponse('externalWalletLogin', requestId, externalWalletLoginResult);
+        break;
+      }
+      case 'fetchWallets': {
+        console.log('Fetching wallets...');
+        const wallets = await para.fetchWallets();
+        sendResponse('fetchWallets', requestId, wallets);
+        break;
+      }
+      case 'generatePasskey': {
+        console.log('Generating passkey...');
+        await generatePasskey(para, args);
+        console.log('Passkey generated successfully.');
+        sendResponse('generatePasskey', requestId, true);
+        break;
+      }
+      case 'generatePasskeyV2': {
+        console.log('Generating passkey V2...');
+        await generatePasskeyV2(para, args);
+        console.log('Passkey V2 generated successfully.');
+        sendResponse('generatePasskeyV2', requestId, true);
+        break;
+      }
+      case 'getEmail': {
+        console.log('Getting email...');
+        const email = para.getEmail();
+        sendResponse('getEmail', requestId, email);
+        break;
+      }
+      case 'getFarcasterConnectURL': {
+        console.log('Getting Farcaster Connect URL...');
+        const farcasterConnectURL = await para.getFarcasterConnectURL();
+        sendResponse('getFarcasterConnectURL', requestId, farcasterConnectURL);
+        break;
+      }
+      case 'getOAuthUrl': {
+        console.log('Getting OAuth URL...');
+        const oAuthUrl = await para.getOAuthURL({ method: args[0] });
+        sendResponse('getOAuthUrl', requestId, oAuthUrl);
+        break;
+      }
+      case 'getPregenWallets': {
+        console.log('Getting pregen wallets...');
+        const pregenWallets = await para.getPregenWallets({ pregenIdentifier: args[0], pregenIdentifierType: args[1] });
+        sendResponse('getPregenWallets', requestId, pregenWallets);
+        break;
+      }
+      case 'getSetUpBiometricsURL': {
+        console.log('Getting set up biometrics URL...');
+        const url = await para.getSetUpBiometricsURL();
+        sendResponse('getSetUpBiometricsURL', requestId, url);
+        break;
+      }
+      case 'getUserShare': {
+        console.log('Getting user share...');
+        const userShare = para.getUserShare();
+        sendResponse('getUserShare', requestId, userShare);
+        break;
+      }
+      case 'getWallets': {
+        console.log('Getting wallets...');
+        const wallets = para.getWallets();
+        sendResponse('getWallets', requestId, wallets);
+        break;
+      }
+      case 'getWalletsByType': {
+        console.log('Getting wallets by type...');
+        const walletsByType = await para.getWalletsByType(args[0]);
+        sendResponse('getWalletsByType', requestId, walletsByType);
+        break;
+      }
+      case 'getWebChallenge': {
+        console.log('Getting web challenge...');
+        const getWebChallengeResult = await para.ctx.client.getWebChallenge(args[0] ?? { email: '' });
+        console.log('Web challenge result:', getWebChallengeResult);
+        sendResponse('getWebChallenge', requestId, getWebChallengeResult);
+        break;
+      }
+      case 'hasPregenWallet': {
+        console.log('Checking if pregen wallet exists...');
+        const hasPregenWallet = await para.hasPregenWallet({ pregenIdentifier: args[0], pregenIdentifierType: args[1] });
+        sendResponse('hasPregenWallet', requestId, hasPregenWallet);
+        break;
+      }
+      case 'initEthersSigner': {
+        await initEthersSigner(para, args);
+        sendResponse('initEthersSigner', requestId, true);
+        break;
+      }
+      case 'isFullyLoggedIn': {
+        console.log('Checking if fully logged in...');
+        const fullyLoggedIn = await para.isFullyLoggedIn();
+        sendResponse('isFullyLoggedIn', requestId, fullyLoggedIn);
+        break;
+      }
+      case 'isSessionActive': {
+        console.log('Checking if session is active...');
+        const sessionActive = await para.isSessionActive();
+        sendResponse('isSessionActive', requestId, sessionActive);
+        break;
+      }
+      case 'login': {
+        console.log('Logging in...');
+        const desiredWallet = await login(para, args);
+        console.log('Login successful. Desired wallet:', desiredWallet);
+        sendResponse('login', requestId, desiredWallet);
+        break;
+      }
+      case 'loginV2': {
+        console.log('Logging in V2...');
+        const desiredWallet2 = await loginV2(para, args);
+        console.log('Login V2 successful. Desired wallet:', desiredWallet2);
+        sendResponse('loginV2', requestId, desiredWallet2);
+        break;
+      }
+      case 'logout': {
+        console.log('Logging out...');
+        await para.logout();
+        sendResponse('logout', requestId, null);
+        break;
+      }
+      case 'resendVerificationCode': {
+        console.log('Resending verification code...');
+        await para.resendVerificationCode();
+        sendResponse('resendVerificationCode', requestId, null);
+        break;
+      }
+      case 'resendVerificationCodeByPhone': {
+        console.log('Resending verification code by phone...');
+        await para.resendVerificationCodeByPhone();
+        sendResponse('resendVerificationCodeByPhone', requestId, null);
+        break;
+      }
+      case 'setEmail': {
+        console.log('Setting email...');
+        await para.setEmail(args[0]);
+        sendResponse('setEmail', requestId, null);
+        break;
+      }
+      case 'setUserShare': {
+        console.log('Setting user share...');
+        await para.setUserShare(args[0]);
+        sendResponse('setUserShare', requestId, null);
+        break;
+      }
+      case 'setup2FA': {
+        console.log('Setting up 2FA...');
+        const setup2FAResult = await para.setup2FA();
+        sendResponse('setup2FA', requestId, setup2FAResult);
+        break;
+      }
+      case 'signMessage': {
+        console.log('Signing message...');
+        const signMessageResult = await para.signMessage({ walletId: args[0], messageBase64: args[1] });
+        sendResponse('signMessage', requestId, signMessageResult);
+        break;
+      }
+      case 'signTransaction': {
+        console.log('Signing transaction...');
+        const signTransactionResult = await para.signTransaction({
+          walletId: args[0],
+          rlpEncodedTxBase64: args[1],
+          chainId: args[2],
+        });
+        sendResponse('signTransaction', requestId, signTransactionResult);
+        break;
+      }
+      case 'updateWalletIdentifierPreGen': {
+        console.log('Updating wallet identifier pre-gen...');
+        await para.updatePregenWalletIdentifier({
+          walletId: args[0],
+          newPregenIdentifier: args[1],
+          newPregenIdentifierType: args[2],
+        });
+        sendResponse('updateWalletIdentifierPreGen', requestId, null);
+        break;
+      }
+      case 'verifyEmail': {
+        console.log('Verifying email...');
+        const verifyEmailResult = await para.verifyEmail({ verificationCode: args[0] });
+        sendResponse('verifyEmail', requestId, verifyEmailResult);
+        break;
+      }
+      case 'verifyPhone': {
+        console.log('Verifying phone...');
+        const verifyPhoneResult = await para.verifyPhone({ verificationCode: args[0] });
+        sendResponse('verifyPhone', requestId, verifyPhoneResult);
+        break;
+      }
+      case 'verifyWebChallenge': {
+        console.log('Verifying web challenge...');
+        const verifyWebChallengeResult = await verifyWebChallenge(para, args);
+        console.log('Web challenge verified:', verifyWebChallengeResult);
+        sendResponse(
+          'verifyWebChallenge',
+          requestId,
+          platform === Platform.iOS ? verifyWebChallengeResult['data']['userId'] : verifyWebChallengeResult,
+        );
+        break;
+      }
+      case 'waitForFarcasterStatus': {
+        console.log('Waiting for Farcaster status...');
+        const waitForFarcasterStatusResult = await para.waitForFarcasterStatus();
+        sendResponse('waitForFarcasterStatus', requestId, waitForFarcasterStatusResult);
+        break;
+      }
+      case 'waitForOAuth': {
+        console.log('Waiting for OAuth...');
+        const waitForOAuthResult = await para.waitForOAuth();
+        sendResponse('waitForOAuth', requestId, waitForOAuthResult);
+        break;
+      }
+      default: {
+        throw new Error(`Method ${methodName} not implemented`);
       }
     }
   } catch (error: any) {
