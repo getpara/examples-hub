caa935c7ee2e2f9436469b31305a4039a936f283
nsquare3 <113948443+nsquare3@users.noreply.github.com>
2025-03-12 10:28:08 -0700

    extra line

--- web/with-svelte-vite/para-modal/src/client/para.ts
+++ web/with-svelte-vite/para-modal/src/client/para.ts
@@ -4,4 +4,3 @@ const API_KEY = import.meta.env.VITE_PARA_API_KEY;
 const ENVIRONMENT = import.meta.env.VITE_PARA_ENVIRONMENT || Environment.BETA
 
 export const para = new ParaWeb(ENVIRONMENT, API_KEY);
-
