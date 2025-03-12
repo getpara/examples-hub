03268bdec98f08d3a349d8e6c0ce16eb73c4c86e
nsquare3 <113948443+nsquare3@users.noreply.github.com>
2025-03-12 10:26:24 -0700

    allow ENVIRONMENT to be passed as env variable

--- web/with-react-nextjs/para-modal/src/client/para.ts
+++ web/with-react-nextjs/para-modal/src/client/para.ts
@@ -1,5 +1,6 @@
 import { Environment, ParaWeb } from "@getpara/react-sdk";
 
 const API_KEY = process.env.NEXT_PUBLIC_PARA_API_KEY;
+const ENVIRONMENT = process.env.NEXT_PUBLIC_PARA_ENVIRONMENT || Environment.BETA;
 
-export const para = new ParaWeb(Environment.BETA, API_KEY);
+export const para = new ParaWeb(ENVIRONMENT, API_KEY);
--- web/with-svelte-vite/para-modal/src/client/para.ts
+++ web/with-svelte-vite/para-modal/src/client/para.ts
@@ -1,5 +1,7 @@
 import { Environment, ParaWeb } from "@getpara/react-sdk";
 
 const API_KEY = import.meta.env.VITE_PARA_API_KEY;
+const ENVIRONMENT = import.meta.env.VITE_PARA_ENVIRONMENT || Environment.BETA
 
-export const para = new ParaWeb(Environment.BETA, API_KEY);
+export const para = new ParaWeb(ENVIRONMENT, API_KEY);
+
