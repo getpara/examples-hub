46a10369050f63c69c97aa5de92ec88270e0dc9f
Tyson <tysonwil@gmail.com>
2025-03-19 17:38:17 -0700

    rm appium tests

--- mobile/with-swift/tests/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    testEnvironment: 'node',
-    transform: {},
-    testTimeout: 900000,
-    setupFilesAfterEnv: ['./jest.setup.js']
-}; 
--- mobile/with-swift/tests/jest.setup.js
+++
@@ -1,5 +0,0 @@
-// Increase timeout for all tests
-jest.setTimeout(900000);
-
-// Suppress console.error during tests
-console.error = jest.fn(); 
