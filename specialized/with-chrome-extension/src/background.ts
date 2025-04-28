7a5dfb59137d3bd122a7550c39bdde1d13d6ca4a
Jordy McNab <jordy@usecapsule.com>
2025-04-28 13:44:37 -0500

    fix: background script and init

--- specialized/with-chrome-extension/src/background.ts
+++ specialized/with-chrome-extension/src/background.ts
@@ -1,28 +1,18 @@
-import { para } from "./client/para";
+import { para, paraReady } from "./client/para";
 
-// Use the background script to open the extension in a new tab if the user is not logged in so that you can use the ParaModal
 chrome.action.onClicked.addListener(async () => {
   try {
+    await paraReady;
     const isLoggedIn = await para.isFullyLoggedIn();
 
+    console.log("User logged in status:", isLoggedIn);
+
     if (!isLoggedIn) {
-      console.log("User is not logged in, opening the extension in a new tab.");
       chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
     } else {
-      console.log("User is logged in, opening the extension in a popup window.");
-      const width = 400;
-      const height = 600;
-      const left = (screen.width - width) / 2;
-      const top = (screen.height - height) / 2;
-
-      chrome.windows.create({
-        url: chrome.runtime.getURL("index.html"),
-        type: "popup",
-        width: width,
-        height: height,
-        left: Math.floor(left),
-        top: Math.floor(top),
-      });
+      await chrome.action.setPopup({ popup: "index.html" });
+      await chrome.action.openPopup();
+      await chrome.action.setPopup({ popup: "" });
     }
   } catch (error) {
     console.error("Authentication check failed:", error);
