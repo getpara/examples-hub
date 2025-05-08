e051228119506a608190648c01ec32a110f56b73
Tyson Williams <tysonwil@gmail.com>
2025-05-07 17:39:07 -0700

    update RN

--- mobile/with-react-native/client/para.ts
+++ mobile/with-react-native/client/para.ts
@@ -1,5 +1,6 @@
+import { PARA_API_KEY } from "@env";
 import { ParaMobile, Environment } from "@getpara/react-native-wallet";
 
-export const para = new ParaMobile(Environment.BETA, process.env.PARA_API_KEY, undefined, {
+export const para = new ParaMobile(Environment.BETA, PARA_API_KEY, undefined, {
   disableWorkers: true,
 });
