18da25eedb7384593127e343f2f794963dac47da
Jordy McNab <jordy@usecapsule.com>
2025-06-27 17:05:15 -0500

    refactor: tanstack exmaple (#93)

--- web/with-react-tanstack-start/para-provider/src/constants.ts
+++
@@ -1,8 +0,0 @@
-import type { Environment } from "@getpara/react-sdk";
-
-export const API_KEY = import.meta.env.VITE_PARA_API_KEY;
-export const ENVIRONMENT = import.meta.env.VITE_PARA_ENVIRONMENT as Environment;
-
-if (!API_KEY) {
-  throw new Error("API key is not defined. Please set VITE_PARA_API_KEY in your environment variables.");
-}
