df917e8a9bc59826453607440a3378b7cfd8e664
Jonny <jonny.howle@gmail.com>
2025-07-02 16:36:59 -0700

    Reduced size of External Wallets installed indicator

--- packages/react-sdk-lite/src/modal/components/ExternalWallets/ExternalWallets.tsx
+++ packages/react-sdk-lite/src/modal/components/ExternalWallets/ExternalWallets.tsx
@@ -192,8 +192,8 @@ const Badge = safeStyled.div<{ $variant: 'installed' | 'mobile'; $show: boolean 
 `;
 
 const InstalledIndicator = safeStyled.span`
-  width: 8px;
-  height: 8px;
+  width: 5px;
+  height: 5px;
   border-radius: 100%;
   background-color: var(--cpsl-color-utility-green);
 `;
