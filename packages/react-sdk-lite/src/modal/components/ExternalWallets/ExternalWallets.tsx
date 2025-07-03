751a0ed2978be285eafb0d4fbd53f2a2bfa54044
Jonny <jonny.howle@gmail.com>
2025-07-02 18:58:20 -0700

    Indicator made smaller

--- packages/react-sdk-lite/src/modal/components/ExternalWallets/ExternalWallets.tsx
+++ packages/react-sdk-lite/src/modal/components/ExternalWallets/ExternalWallets.tsx
@@ -192,8 +192,8 @@ const Badge = safeStyled.div<{ $variant: 'installed' | 'mobile'; $show: boolean 
 `;
 
 const InstalledIndicator = safeStyled.span`
-  width: 5px;
-  height: 5px;
+  width: 4px;
+  height: 4px;
   border-radius: 100%;
   background-color: var(--cpsl-color-utility-green);
 `;
