9e928211009e8a332cf0f7e2a34ff37ee79273c0
Jordy McNab <jordy@usecapsule.com>
2025-09-05 19:17:06 -0500

    refactor: simplify QueryClient initialization in Providers component

--- web/with-react-nextjs/para-modal/src/context/Providers.tsx
+++ web/with-react-nextjs/para-modal/src/context/Providers.tsx
@@ -4,14 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ParaProvider as ParaSDKProvider } from "@getpara/react-sdk";
 import { API_KEY, ENVIRONMENT } from "@/config/constants";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5,
-      gcTime: 1000 * 60 * 30,
-    },
-  },
-});
+const queryClient = new QueryClient();
 
 export function Providers({
   children,
